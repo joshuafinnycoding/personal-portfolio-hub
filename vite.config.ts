7b11f454a8666a9a19dd78765d7c8268af3ed183
joshuafinnycoding <joshuafinny@gmail.com>
2025-07-23 11:29:47 +0530

    Third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import path from "path";
 
 export default defineConfig({
+  base: "/personal-portfolio-hub/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
